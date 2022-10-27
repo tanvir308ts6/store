f51f652d5971b25b34677ec751c87c04803aeeca
tanvir <tanvir@eatlbd.com>
2022-10-27 18:58:39 +0600

    OK

--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './Routes'
+import store from './store/store'
+import vuetify from '@/plugins/vuetify'
 // import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 // import 'bootstrap-vue/dist/bootstrap-vue.css'
 import "bootstrap/dist/css/bootstrap.css"
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 
-// Vue.use(BootstrapVue)
-
-// Vue.use(IconsPlugin)
-
-createApp(App).use(router).mount('#app')
+createApp(App).use(router,store).mount('#app')
 
 import "bootstrap/dist/js/bootstrap.bundle"
 <script src="https://kit.fontawesome.com/8d5aa983d2.js" crossorigin="anonymous"></script>
